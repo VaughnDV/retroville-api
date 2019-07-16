6229dfe7acb623e3422533cd1e67aacfcbaaceb0
Vaughn De Villiers <vdevilliers@morneaushepell.com>
2019-07-16 22:06:54 +0100

    ci(travis) Testing

--- retroville/urls.py
+++ retroville/urls.py
@@ -25,8 +25,6 @@ urlpatterns = [
     path("api/v1/", include(router.urls)),
     path("api-token-auth/", views.obtain_auth_token),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
-    # the 'api-root' from django rest-frameworks default router
-    # http://www.django-rest-framework.org/api-guide/routers/#defaultrouter
     re_path(r"^$", RedirectView.as_view(url=reverse_lazy("api-root"), permanent=False)),
     path("accessToken/", token, name="token"),
     path("placeCall/", placeCall, name="placeCall"),
