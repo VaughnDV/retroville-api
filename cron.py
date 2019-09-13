079a4ab408edf1f1aa6efe280da11d254f6a3ded
VaughnDV <vaughndevilliers@gmail.com>
2019-09-13 18:52:40 +0100

    Working on cron

--- cron.py
+++ cron.py
@@ -20,42 +20,45 @@ def fetch_stories():
     url = "https://newsapi.org/v2/top-headlines"
     checked = []
     checked_titles = []
-    catagories = ["business", "business", "health", "health", "technology", "science", "sports", "entertainment", "general", "general"]
+    categories = ["business", "business", "health", "health", "technology", "science", "sports", "entertainment", "general", "general"]
 
-    for catagory in catagories:
+    for category in categories:
+        try:
+            request_url = f"{url}?country={COUNTRY}&category={category}&sortBy=popularity&language={LANGUAGE}&apiKey={NEWS_API_KEY}"
+            title = None
+            content = None
+            index = checked.count(f"{category}")
 
-        request_url = f"{url}?country={COUNTRY}&category={catagory}&sortBy=popularity&language={LANGUAGE}&apiKey={NEWS_API_KEY}"
-        print(request_url)
-        print("#" * 50)
+            response = requests.get(request_url)
 
-        title = None
-        content = None
-        # index = checked.count(f"{catagory}")
+            articles = json.loads(response.content).get("articles", "")
 
-        response = requests.get(request_url)
-        pprint(response.content)
-
-        articles = json.loads(response.content)["articles"]
-
-        for artitle in articles:
-            if artitle["title"] and artitle["title"] not in checked_titles:
-                title = artitle["title"]
-            else:
+            if not articles:
+                print(json.loads(response.content)["message"])
                 continue
-            if artitle["content"]:
-                content = artitle["content"].rstrip()
-            else:
-                continue
-            picture_url = artitle["urlToImage"]
+
+            while not title and not content:
+                if articles[index]["title"] and articles[index]["title"] not in checked_titles:
+                    if articles[index]["content"] and len(articles[index]["content"]) > 20:
+                        title = articles[index]["title"]
+                        checked_titles.append(title)
+                        content = articles[index]["content"].rstrip()
+                        print("#" * 50)
+                        print(title)
+                        print("#" * 50)
+
+            picture_url = articles[index]["urlToImage"]
             live_date = date.today().strftime("%Y-%m-%d")
-    #
-    #     Story.objects.create(
-    #         title=title,
-    #         content=content,
-    #         picture_url=picture_url,
-    #         live_date=live_date
-    #     )
-    #     checked.append(catagory)
+
+            Story.objects.create(
+                title=title,
+                content=content,
+                picture_url=picture_url,
+                live_date=live_date
+            )
+            checked.append(category)
+        except Exception as e:
+            print(e)
 
 
 if __name__ == '__main__':
@@ -64,7 +67,7 @@ if __name__ == '__main__':
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "retroville.config")
     COUNTRY = os.environ.get("COUNTRY", "gb")
     LANGUAGE = os.environ.get("LANGUAGE", "en")
-    NEWS_API_KEY = os.environ.get("NEWS_API_KETY", "c5c5322336d54e079ba396b59d850e52")
+    NEWS_API_KEY = os.environ.get("NEWS_API_KEY", "c5c5322336d54e079ba396b59d850e52")
     import configurations
     configurations.setup()
     from retroville.stories.models import Story
