1aa891a2437710002e1794f597b709b38c8b9253
VaughnDV <vaughndevilliers@gmail.com>
2019-09-15 23:14:49 +0100

    Changes to cron

--- cron.py
+++ cron.py
@@ -13,43 +13,52 @@ def fetch_stories():
     checked = []
     checked_titles = []
     categories = ["business", "business", "health", "health", "technology", "science", "sports", "entertainment", "general", "general"]
+    count = 0
+    while count <= 10:
+        for category in categories:
+            try:
+                request_url = f"{url}?country={COUNTRY}&category={category}&sortBy=popularity&language={LANGUAGE}&apiKey={NEWS_API_KEY}"
+                title = None
+                content = None
+                limit = 10
+                index = checked.count(f"{category}")
 
-    for category in categories:
-        try:
-            request_url = f"{url}?country={COUNTRY}&category={category}&sortBy=popularity&language={LANGUAGE}&apiKey={NEWS_API_KEY}"
-            title = None
-            content = None
-            limit = 10
-            index = checked.count(f"{category}")
+                response = requests.get(request_url)
 
-            response = requests.get(request_url)
+                if not response.ok:
+                    print(response.reason)
+                    print(response.status_code)
+                    continue
 
-            articles = json.loads(response.content).get("articles", "")
+                articles = json.loads(response.content).get("articles", "")
 
-            if not articles:
-                print(json.loads(response.content)["message"])
+                if not articles:
+                    print(json.loads(response.content)["message"])
+                    continue
+
+                while not title and not content and limit > 0:
+                    if articles[index]["title"] and articles[index]["title"] not in checked_titles:
+                        if articles[index]["url"]:
+                            title = articles[index]["title"]
+                            checked_titles.append(title)
+                            content = articles[index]["url"]
+                            limit -= 1
+
+                picture_url = articles[index]["urlToImage"]
+                live_date = date.today().strftime("%Y-%m-%d")
+
+                Story.objects.create(
+                    title=title,
+                    content=content,
+                    picture_url=picture_url,
+                    live_date=live_date
+                )
+                checked.append(category)
+                count += 1
+
+            except Exception as e:
+                print(e)
                 continue
-
-            while not title and not content and limit > 0:
-                if articles[index]["title"] and articles[index]["title"] not in checked_titles:
-                    if articles[index]["url"] and len(articles[index]["url"]) > 20:
-                        title = articles[index]["title"]
-                        checked_titles.append(title)
-                        content = articles[index]["url"].rstrip()
-                        limit -= 1
-
-            picture_url = articles[index]["urlToImage"]
-            live_date = date.today().strftime("%Y-%m-%d")
-
-            Story.objects.create(
-                title=title,
-                content=content,
-                picture_url=picture_url,
-                live_date=live_date
-            )
-            checked.append(category)
-        except Exception as e:
-            print(e)
 
 
 if __name__ == '__main__':
